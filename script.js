75cd7bdb224a0e4c2526b29226a1693dddae070f
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-10-11 12:51:48 -0300

    Create a new variable and style it.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
 for (let i = 0; i < 16; i++) {
+    let divRow = document.createElement('div');
     for (let j = 0; j < 16; j++) {
-        let div = document.createElement("div");
-        container.appendChild(div).classList.add("divChild");
+        let divChild = document.createElement('div');
+        divRow.appendChild(divChild).classList.add('divChild');
     }
+    container.appendChild(divRow).classList.add('divRow');
 }
