6e603d6b55f27da07417977f9d35d2f5b8a52ca0
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-10-12 10:51:31 -0300

    Create a function to make the grid

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('.btn');
 
-for (let i = 0; i < 16; i++) {
-    let divRow = document.createElement('div');
-    for (let j = 0; j < 16; j++) {
-        let divChild = document.createElement('div');
-        divRow.appendChild(divChild).classList.add('divChild');
-        divChild.addEventListener('mouseover', function(e) {
-            e.target.style.background = 'black';
-        });
-    }
-    container.appendChild(divRow).classList.add('divRow');
+btn.addEventListener('click', () => {
+    alert('Do you want to change the grid?');
+    
+});
+function createSquares(squares = 16) {
+    for (let i = 0; i < squares; i++) {
+        let divRow = document.createElement('div');
+        for (let j = 0; j < squares; j++) {
+            let divChild = document.createElement('div');
+            divRow.appendChild(divChild).classList.add('divChild');
+            divChild.addEventListener('mouseover', function(e) {
+                e.target.style.background = 'black';
+            });
+        }
+        container.appendChild(divRow).classList.add('divRow');
+    }    
 }
+createSquares();
