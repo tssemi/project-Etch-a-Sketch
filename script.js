30beda346ad1aaa81fe5bd5c5f2ee8df2f45b759
Tssemi <127999956+tssemi@users.noreply.github.com>
2024-10-12 12:59:19 -0300

    Add an event listener and a new variable to change opacity

--- script.js
+++ script.js
@@ -2,20 +2,36 @@ const container = document.querySelector('.container');
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
-    alert('Do you want to change the grid?');
-    
+    let removeDiv = document.querySelectorAll('.divRow');
+    for (let e of removeDiv) e.remove();
+    let num;
+    do
+    num = prompt('Put the number of squares per side. The limit of squares is 100', );
+    while (num > 100);
+    createSquares(num);
 });
 function createSquares(squares = 16) {
     for (let i = 0; i < squares; i++) {
         let divRow = document.createElement('div');
         for (let j = 0; j < squares; j++) {
+            let opa = 0.1;
             let divChild = document.createElement('div');
             divRow.appendChild(divChild).classList.add('divChild');
-            divChild.addEventListener('mouseover', function(e) {
+            divChild.addEventListener('mouseover', (e) => {
                 e.target.style.background = 'black';
+                e.target.style.opacity = opa;
+                opa += 0.1;
             });
+            divChild.addEventListener('click', (e) => {
+                e.target.style.background = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+                e.target.style.opacity = '100%';
+            })
         }
         container.appendChild(divRow).classList.add('divRow');
     }    
+}
+
+function randomColor() {
+    return Math.floor(Math.random() * 255)
 }
 createSquares();
